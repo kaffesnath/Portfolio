c21d79365a1c82130b3746247b3473c6685c958c
kaffesnath <kaffesnath@myvuw.ac.nz>
2025-08-29 11:41:24 +1200

    Updated font family to match resume

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
 
     return (
         <div id="mainBody" className="h-dvh w-full overflow-hidden relative">
-            <div className="font-firmaAlt text-title font-black flex h-1/10 bg-[#f3f3f3] text-[#231f20] items-center justify-center">Nathan Kaffes.</div>
+            <div className="font-lexend text-title font-semibold flex h-1/10 bg-[#f3f3f3] text-[#231f20] items-center justify-center">Nathan Kaffes.</div>
             <div ref={containerRef} className="touch-none overflow-hidden h-screen relative">
                 <ReactP5Wrapper sketch={sketch}/>
             </div>
